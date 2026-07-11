5831110314258cb14b39bfcf44e6ffb98888db54
Jhon Michael Cuenca Jaramillo <djxmaicolx@Mac.fritz.box>
2026-07-11 16:40:39 +0200

    Add Europass qualification documentation

--- assets/js/language.js
+++ assets/js/language.js
@@ -25,7 +25,7 @@
 
 const Language = (() => {
     const STORAGE_KEY = 'portfolio-language';
-    const TRANSLATION_VERSION = '20260709-german-active';
+    const TRANSLATION_VERSION = '20260711-europass-felasa';
     const DEFAULT_LANGUAGE = 'es';
     const AVAILABLE_LANGUAGES = ['en', 'de', 'es'];
     
